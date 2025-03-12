3454c41163622e5ae8167acd19670e080d136ccd
Artem Nazarko <nazarko.artem@chnu.edu.ua>
2025-03-12 21:26:43 +0200

    Исправлены пути к изображениям

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import ProfileModal from './components/ProfileModal';
 const products = [
   // NVIDIA 4000 Series
   { id: 1, name: "RTX 4090", brand: "NVIDIA", price: 1599, imageUrl: "https://images.prom.ua/6310607944_w640_h640_videokarta-asus-rog.jpg" },
-  { id: 2, name: "GeForce RTX 4080 SUPER", brand: "NVIDIA", price: 1199, imageUrl: "/images/4080.webp" },
-  { id: 3, name: "RTX 4070 Ti 16GB GDDR6X", brand: "NVIDIA", price: 899, imageUrl: "/images/4070Ti.webp" },
-  { id: 4, name: "RTX 4070 12GB GDDR6X", brand: "NVIDIA", price: 599, imageUrl: "/images/4070.webp" },
-  { id: 5, name: "RTX 4060 Ti 16GB GDDR6", brand: "NVIDIA", price: 499, imageUrl: "/images/4060Ti.webp" },
-  { id: 6, name: "GeForce RTX 4060 8GB GDDR6", brand: "NVIDIA", price: 399, imageUrl: "/images/4060.webp" },
+  { id: 2, name: "GeForce RTX 4080 SUPER", brand: "NVIDIA", price: 1199, imageUrl: "https://content.rozetka.com.ua/goods/images/big/340992158.jpg" },
+  { id: 3, name: "RTX 4070 Ti 16GB GDDR6X", brand: "NVIDIA", price: 899, imageUrl: "https://img.mta.ua/image/cache/data/foto/z869/869196/photos/MSI-Nvidia-GeForce-RTX-4070-GAMING-SLIM-12G-23-Picture-01-600x600.jpg" },
+  { id: 4, name: "RTX 4070 12GB GDDR6X", brand: "NVIDIA", price: 599, imageUrl: "https://click.ua/content/shop/products/144090/photos/videokarta-gf-rtx-4070-12gb-gddr6x-ventus-3x-e1-oc-msi-geforce-rtx-4070-ventus-3x-e1-12g-oc-800x800-f0ba.jpg" },
+  { id: 5, name: "RTX 4060 Ti 16GB GDDR6", brand: "NVIDIA", price: 499, imageUrl: "https://img.telemart.ua/526407-675389-product_popup/msi-geforce-rtx-4060-ti-gaming-x-slim-16384mb-rtx-4060-ti-gaming-x-slim-16g.png" },
+  { id: 6, name: "GeForce RTX 4060 8GB GDDR6", brand: "NVIDIA", price: 399, imageUrl: "https://cdn.comfy.ua/media/catalog/product/cache/4/image/600x/9df78eab33525d08d6e5fb8d27136e95/g/e/geforce_rtx_173_173_4060_gaming_oc_8g-07.jpg" },
 
   // NVIDIA 5000 Series
   { id: 8, name: "RTX 5090", brand: "NVIDIA", price: 2499, imageUrl: "https://img.telemart.ua/721174-896470-product_popup/asus-rog-astral-geforce-rtx-5090-oc-32768mb-rog-astral-rtx5090-o32g-gaming.jpg" },
@@ -39,7 +39,7 @@ export default function App() {
   const [cart, setCart] = useState([]);
   const [filter, setFilter] = useState("");
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [isProfileOpen, setIsProfileOpen] = useState(false); // Состояние для модального окна профиля
+  const [isProfileOpen, setIsProfileOpen] = useState(false); 
 
   const addToCart = (product) => {
     setCart([...cart, { ...product, quantity: 1 }]);
